6b3c40c903bdaf2d67961aba493418a44bec95a6
Maxim270693 <maxim.zhuk.93@mail.ru>
2021-07-27 18:39:47 +0300

    backend setup

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-
-app.get('/', (req, res) => {
-    res.send('Hello it is WS server');
+const {Server} = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
 });
+const cors = require('cors');
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on("c", (message) => {
+        console.log(message)
+    })
+});
+
 
 server.listen(3009, () => {
     console.log('listening on *:3009');
